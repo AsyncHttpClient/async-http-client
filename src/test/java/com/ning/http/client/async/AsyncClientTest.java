d48ab8c3227cc3751a9e5bc5a9bf0c0972b42a0a
Jeanfrancois Arcand <jfarcand@apache.org>
2010-03-19 12:10:08 -0400

    Add a unit test which cover issue 8, which does not seems to be an issue. Note that we already have unit test for setMaxConnection

--- src/test/java/com/ning/http/client/async/AsyncClientTest.java
+++ src/test/java/com/ning/http/client/async/AsyncClientTest.java
@@ -1,12 +1,16 @@
 package com.ning.http.client.async;
 
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Response;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
 
 public class AsyncClientTest extends AbstractBasicTest {
 
@@ -31,4 +35,32 @@ public class AsyncClientTest extends AbstractBasicTest {
         assertEquals(response.getResponseBody(), body);
     }
 
+    @Test
+    public void multipleMaxConnectionOpenTest() throws Throwable {
+        AsyncHttpClientConfig cg = new AsyncHttpClientConfig.Builder().setKeepAlive(true)
+                .setConnectionTimeoutInMs(1000).setMaximumConnectionsTotal(1).build();
+        AsyncHttpClient c = new AsyncHttpClient(cg);
+
+        String body = "hello there";
+
+        // once
+        Response response = c.preparePost(TARGET_URL)
+                .setBody(body)
+                .execute().get(5, TimeUnit.SECONDS);
+
+        assertEquals(response.getResponseBody(), body);
+
+        // twice
+        Exception exception = null;
+        try{
+            response = c.preparePost(TARGET_URL)
+                    .setBody(body)
+                    .execute().get(5, TimeUnit.SECONDS);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            exception = ex;
+        }
+        assertNotNull(exception);
+        assertEquals(exception.getMessage(),"Too many connections");
+    }
 }
