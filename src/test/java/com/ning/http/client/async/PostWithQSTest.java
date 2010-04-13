4d1bce3164021412f95be2f999391d29f3eda3d8
Hubert Iwaniuk <neotyk@kungfoo.pl>
2010-04-14 01:08:55 +0200

    test case for http://github.com/ning/async-http-client/issues#issue/23

---
+++ src/test/java/com/ning/http/client/async/PostWithQSTest.java
@@ -0,0 +1,84 @@
+package com.ning.http.client.async;
+
+import com.ning.http.client.AsyncCompletionHandlerBase;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Request;
+import com.ning.http.client.RequestBuilder;
+import com.ning.http.client.RequestType;
+import com.ning.http.client.Response;
+import org.apache.log4j.BasicConfigurator;
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * Tests POST request with Query String.
+ *
+ * @author Hubert Iwaniuk
+ */
+public class PostWithQSTest extends AbstractBasicTest {
+
+    /** POST with QS server part. */
+    private class PostWithQSHandler extends AbstractHandler {
+        public void handle(String s,
+                           HttpServletRequest request,
+                           HttpServletResponse response,
+                           int i) throws IOException, ServletException {
+            if ("POST".equalsIgnoreCase(request.getMethod())) {
+                String qs = request.getQueryString();
+                if (qs != null && !qs.isEmpty() && request.getInputStream().available() == 3) {
+                    response.setStatus(HttpServletResponse.SC_OK);
+                } else {
+                    response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                }
+            } else { // this handler is to handle POST request
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            }
+        }
+    }
+
+    @Test(enabled = false)
+    public void testHEAD302() throws IOException, BrokenBarrierException, InterruptedException, ExecutionException, TimeoutException {
+        AsyncHttpClient client = new AsyncHttpClient();
+        Future<Response> f = client.preparePost("http://localhost:" + PORT + "/?a=b").setBody("abc".getBytes()).execute();
+        Response resp = f.get(3, TimeUnit.SECONDS);
+        assertNotNull(resp);
+        assertEquals(resp.getStatusCode(), HttpServletResponse.SC_OK);
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpGlobal() throws Exception {
+        server = new Server();
+        BasicConfigurator.configure();
+
+        Connector listener = new SelectChannelConnector();
+
+        listener.setHost("127.0.0.1");
+        listener.setPort(PORT);
+        server.addConnector(listener);
+
+        server.setHandler(new PostWithQSHandler());
+        server.start();
+        log.info("Local HTTP server started successfully");
+    }
+}
