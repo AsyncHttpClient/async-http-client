ffb96835136342d696294fc0b82d8088a92847f7
jfarcand <jfarcand@apache.org>
2010-08-30 17:48:27 -0400

    Fix build failure with JDK 5

--- src/test/java/com/ning/http/client/async/AsyncProvidersBasicTest.java
+++ src/test/java/com/ning/http/client/async/AsyncProvidersBasicTest.java
@@ -1373,7 +1373,7 @@ public class AsyncProvidersBasicTest extends AbstractBasicTest {
     public void testAwsS3() throws Exception {
         final AsyncHttpClient c = new AsyncHttpClient(new Builder().build());
         Response response = c.prepareGet("http://test.s3.amazonaws.com/").execute().get();
-        if (response.getResponseBody() == null || response.getResponseBody().isEmpty()) {
+        if (response.getResponseBody() == null || response.getResponseBody().equals("")) {
             fail("No response Body");
         } else {
             assertEquals(response.getStatusCode(), 403);
