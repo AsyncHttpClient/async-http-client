a70b8dc0a6ad5de8ddfa62d5e5f598fb8174d667
jfarcand <jfarcand@apache.org>
2010-10-04 14:32:23 -0400

    Improve Cookie class.

--- src/main/java/com/ning/http/client/Cookie.java
+++ src/main/java/com/ning/http/client/Cookie.java
@@ -16,13 +16,20 @@
  */
 package com.ning.http.client;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Cookie {
-    private String domain;
-    private String name;
-    private String value;
-    private String path;
-    private int maxAge;
-    private boolean secure;
+    private final String domain;
+    private final String name;
+    private final String value;
+    private final String path;
+    private final int maxAge;
+    private final boolean secure;
+    private final int version;
+    private Set<Integer> ports = Collections.emptySet();
+    private Set<Integer> unmodifiablePorts = ports;
 
     public Cookie(String domain, String name, String value, String path, int maxAge, boolean secure) {
         this.domain = domain;
@@ -31,6 +38,17 @@ public class Cookie {
         this.path = path;
         this.maxAge = maxAge;
         this.secure = secure;
+        this.version = 1;
+    }
+
+    public Cookie(String domain, String name, String value, String path, int maxAge, boolean secure, int version) {
+        this.domain = domain;
+        this.name = name;
+        this.value = value;
+        this.path = path;
+        this.maxAge = maxAge;
+        this.secure = secure;
+        this.version = version;
     }
 
     public String getDomain() {
@@ -55,6 +73,54 @@ public class Cookie {
 
     public boolean isSecure() {
         return secure;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public Set<Integer> getPorts() {
+        if (unmodifiablePorts == null) {
+            unmodifiablePorts = Collections.unmodifiableSet(ports);
+        }
+        return unmodifiablePorts;
+    }
+
+    public void setPorts(int... ports) {
+        if (ports == null) {
+            throw new NullPointerException("ports");
+        }
+
+        int[] portsCopy = ports.clone();
+        if (portsCopy.length == 0) {
+            unmodifiablePorts = this.ports = Collections.emptySet();
+        } else {
+            Set<Integer> newPorts = new TreeSet<Integer>();
+            for (int p: portsCopy) {
+                if (p <= 0 || p > 65535) {
+                    throw new IllegalArgumentException("port out of range: " + p);
+                }
+                newPorts.add(Integer.valueOf(p));
+            }
+            this.ports = newPorts;
+            unmodifiablePorts = null;
+        }
+    }
+
+    public void setPorts(Iterable<Integer> ports) {
+        Set<Integer> newPorts = new TreeSet<Integer>();
+        for (int p: ports) {
+            if (p <= 0 || p > 65535) {
+                throw new IllegalArgumentException("port out of range: " + p);
+            }
+            newPorts.add(Integer.valueOf(p));
+        }
+        if (newPorts.isEmpty()) {
+            unmodifiablePorts = this.ports = Collections.emptySet();
+        } else {
+            this.ports = newPorts;
+            unmodifiablePorts = null;
+        }
     }
 
     @Override
