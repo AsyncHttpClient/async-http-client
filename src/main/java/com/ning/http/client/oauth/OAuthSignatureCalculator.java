459431a1bc63aa140ca61ff67af89a7eacc387ca
jfarcand <jfarcand@apache.org>
2010-10-12 09:03:23 -0400

    Add missing headers/license, reformat. No functional changes

--- src/main/java/com/ning/http/client/oauth/OAuthSignatureCalculator.java
+++ src/main/java/com/ning/http/client/oauth/OAuthSignatureCalculator.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2010 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package com.ning.http.client.oauth;
 
-import java.util.*;
 
 import com.ning.http.client.FluentStringsMap;
 import com.ning.http.client.Request;
@@ -10,52 +25,56 @@ import com.ning.http.util.Base64;
 import com.ning.http.util.UTF8Codec;
 import com.ning.http.util.UTF8UrlEncoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Simple OAuth signature calculator that can used for constructing client signatures
  * for accessing services that use OAuth for authorization.
- *<p>
+ * <p/>
  * Supports most common signature inclusion and calculation methods: HMAC-SHA1 for
  * calculation, and Header inclusion as inclusion method. Nonce generation uses
  * simple random numbers with base64 encoding.
- * 
+ *
  * @author tatu (tatu.saloranta@iki.fi)
  */
 public class OAuthSignatureCalculator
-    implements SignatureCalculator
-{
+        implements SignatureCalculator {
     public final static String HEADER_AUTHORIZATION = "Authorization";
 
-    private final String KEY_OAUTH_CONSUMER_KEY =   "oauth_consumer_key";
-    private final String KEY_OAUTH_NONCE =          "oauth_nonce";
-    private final String KEY_OAUTH_SIGNATURE =      "oauth_signature";
-    private final String KEY_OAUTH_SIGNATURE_METHOD="oauth_signature_method";
-    private final String KEY_OAUTH_TIMESTAMP =      "oauth_timestamp";
-    private final String KEY_OAUTH_TOKEN =          "oauth_token";
-    private final String KEY_OAUTH_VERSION =        "oauth_version";
-    
+    private final String KEY_OAUTH_CONSUMER_KEY = "oauth_consumer_key";
+    private final String KEY_OAUTH_NONCE = "oauth_nonce";
+    private final String KEY_OAUTH_SIGNATURE = "oauth_signature";
+    private final String KEY_OAUTH_SIGNATURE_METHOD = "oauth_signature_method";
+    private final String KEY_OAUTH_TIMESTAMP = "oauth_timestamp";
+    private final String KEY_OAUTH_TOKEN = "oauth_token";
+    private final String KEY_OAUTH_VERSION = "oauth_version";
+
     private final String OAUTH_VERSION_1_0 = "1.0";
     private final String OAUTH_SIGNATURE_METHOD = "HMAC-SHA1";
-    
+
     /**
      * To generate Nonce, need some (pseudo)randomness; no need for
      * secure variant here.
      */
     protected final Random random;
-    
+
     protected final byte[] nonceBuffer = new byte[16];
-    
+
     protected final ThreadSafeHMAC mac;
 
     protected final ConsumerKey consumerAuth;
-    
+
     protected final RequestToken userAuth;
 
     /**
      * @param consumerAuth Consumer key to use for signature calculation
-     * @param userAuth Request/access token to use for signature calculation
+     * @param userAuth     Request/access token to use for signature calculation
      */
-    public OAuthSignatureCalculator(ConsumerKey consumerAuth, RequestToken userAuth)
-    {
+    public OAuthSignatureCalculator(ConsumerKey consumerAuth, RequestToken userAuth) {
         mac = new ThreadSafeHMAC(consumerAuth, userAuth);
         this.consumerAuth = consumerAuth;
         this.userAuth = userAuth;
@@ -63,6 +82,7 @@ public class OAuthSignatureCalculator
     }
 
     //@Override // silly 1.5; doesn't allow this for interfaces
+
     public void calculateAndAddSignature(String baseURL, Request request, RequestBuilderBase<?> requestBuilder) {
         String method = request.getReqType().toString(); // POST etc
         String nonce = generateNonce();
@@ -76,8 +96,7 @@ public class OAuthSignatureCalculator
      * Method for calculating OAuth signature using HMAC/SHA-1 method.
      */
     public String calculateSignature(String method, String baseURL, long oauthTimestamp, String nonce,
-            FluentStringsMap formParams, FluentStringsMap queryParams)
-    {
+                                     FluentStringsMap formParams, FluentStringsMap queryParams) {
         StringBuilder signedText = new StringBuilder(100);
         signedText.append(method); // POST / GET etc (nothing to URL encode)
         signedText.append('&');
@@ -88,13 +107,13 @@ public class OAuthSignatureCalculator
         if (baseURL.startsWith("http:")) {
             int i = baseURL.indexOf(":80/", 4);
             if (i > 0) {
-                baseURL = baseURL.substring(0, i) + baseURL.substring(i+3);
-            }                
+                baseURL = baseURL.substring(0, i) + baseURL.substring(i + 3);
+            }
         } else if (baseURL.startsWith("https:")) {
             int i = baseURL.indexOf(":443/", 5);
             if (i > 0) {
-                baseURL = baseURL.substring(0, i) + baseURL.substring(i+4);
-            }                
+                baseURL = baseURL.substring(0, i) + baseURL.substring(i + 4);
+            }
         }
         signedText.append(UTF8UrlEncoder.encode(baseURL));
 
@@ -103,7 +122,7 @@ public class OAuthSignatureCalculator
          * for calculating request signature
          */
         OAuthParameterSet allParameters = new OAuthParameterSet();
-        
+
         // start with standard OAuth parameters we need
         allParameters.add(KEY_OAUTH_CONSUMER_KEY, consumerAuth.getKey());
         allParameters.add(KEY_OAUTH_NONCE, nonce);
@@ -129,11 +148,11 @@ public class OAuthSignatureCalculator
             }
         }
         String encodedParams = allParameters.sortAndConcat();
-        
+
         // and all that needs to be URL encoded (... again!)
         signedText.append('&');
         UTF8UrlEncoder.appendEncoded(signedText, encodedParams);
-        
+
         byte[] rawBase = UTF8Codec.toUTF8(signedText.toString());
         byte[] rawSignature = mac.digest(rawBase);
         // and finally, base64 encoded... phew!
@@ -141,10 +160,9 @@ public class OAuthSignatureCalculator
     }
 
     /**
-     * Method used for constructing 
+     * Method used for constructing
      */
-    public String constructAuthHeader(String signature, String nonce, long oauthTimestamp)
-    {
+    public String constructAuthHeader(String signature, String nonce, long oauthTimestamp) {
         StringBuilder sb = new StringBuilder(200);
         sb.append("OAuth ");
         sb.append(KEY_OAUTH_CONSUMER_KEY).append("=\"").append(consumerAuth.getKey()).append("\", ");
@@ -165,8 +183,7 @@ public class OAuthSignatureCalculator
         return sb.toString();
     }
 
-    private synchronized String generateNonce()
-    {
+    private synchronized String generateNonce() {
         random.nextBytes(nonceBuffer);
         // let's use base64 encoding over hex, slightly more compact than hex or decimals
         return Base64.encode(nonceBuffer);
@@ -181,25 +198,23 @@ public class OAuthSignatureCalculator
      * by key, which usually has nothing to encode)... of course, rarely means that
      * when it would occur it'd be harder to track down.
      */
-    final static class OAuthParameterSet
-    {
+    final static class OAuthParameterSet {
         final private ArrayList<Parameter> allParameters = new ArrayList<Parameter>();
-        
-        public OAuthParameterSet() { }
 
-        public OAuthParameterSet add(String key, String value)
-        {
-            Parameter p =  new Parameter(UTF8UrlEncoder.encode(key), UTF8UrlEncoder.encode(value));
+        public OAuthParameterSet() {
+        }
+
+        public OAuthParameterSet add(String key, String value) {
+            Parameter p = new Parameter(UTF8UrlEncoder.encode(key), UTF8UrlEncoder.encode(value));
             allParameters.add(p);
             return this;
         }
 
-        public String sortAndConcat()
-        {
+        public String sortAndConcat() {
             // then sort them (AFTER encoding, important)
             Parameter[] params = allParameters.toArray(new Parameter[allParameters.size()]);
             Arrays.sort(params);
-    
+
             // and build parameter section using pre-encoded pieces:
             StringBuilder encodedParams = new StringBuilder(100);
             for (Parameter param : params) {
@@ -211,25 +226,29 @@ public class OAuthSignatureCalculator
             return encodedParams.toString();
         }
     }
-    
+
     /**
      * Helper class for sorting query and form parameters that we need
      */
-    final static class Parameter implements Comparable<Parameter>
-    {
+    final static class Parameter implements Comparable<Parameter> {
         private final String key, value;
-        
+
         public Parameter(String key, String value) {
             this.key = key;
             this.value = value;
         }
 
-        public String key() { return key; }
-        public String value() { return value; }
+        public String key() {
+            return key;
+        }
+
+        public String value() {
+            return value;
+        }
 
         //@Override // silly 1.5; doesn't allow this for interfaces
-        public int compareTo(Parameter other)
-        {
+
+        public int compareTo(Parameter other) {
             int diff = key.compareTo(other.key);
             if (diff == 0) {
                 diff = value.compareTo(other.value);
@@ -237,7 +256,8 @@ public class OAuthSignatureCalculator
             return diff;
         }
 
-        @Override public String toString() {
+        @Override
+        public String toString() {
             return key + "=" + value;
         }
     }
