a1b9a19428ecf58422fce03dc8ad81b12d9ad930
jfarcand <jfarcand@apache.org>
2010-10-28 15:49:17 -0400

    Check for null

--- src/main/java/com/ning/http/client/providers/jdk/JDKFuture.java
+++ src/main/java/com/ning/http/client/providers/jdk/JDKFuture.java
@@ -56,7 +56,9 @@ public class JDKFuture<V> implements FutureImpl<V> {
     }
 
     public void abort(Throwable t) {
-        innerFuture.cancel(true);
+        if (innerFuture != null) {
+            innerFuture.cancel(true);
+        }
         exception.set(t);
         if (!timedOut.get() && !cancelled.get()) {
             asyncHandler.onThrowable(t);
@@ -65,11 +67,19 @@ public class JDKFuture<V> implements FutureImpl<V> {
 
     public boolean cancel(boolean mayInterruptIfRunning) {
         cancelled.set(true);
-        return innerFuture.cancel(mayInterruptIfRunning);
+        if (innerFuture != null) {
+            return innerFuture.cancel(mayInterruptIfRunning);
+        } else {
+            return false;
+        }
     }
 
     public boolean isCancelled() {
-        return innerFuture.isCancelled();
+        if (innerFuture != null) {
+            return innerFuture.isCancelled();
+        } else {
+            return false;
+        }
     }
 
     public boolean isDone() {
@@ -88,7 +98,9 @@ public class JDKFuture<V> implements FutureImpl<V> {
     public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
         V content = null;
         try {
-            content = innerFuture.get(timeout, unit);
+            if (innerFuture != null) {
+                content = innerFuture.get(timeout, unit);
+            }
         } catch (TimeoutException t) {
             if (!contentProcessed.get() && timeout != -1 && ((System.currentTimeMillis() - touch.get()) <= responseTimeoutInMs)) {
                 return get(timeout,unit);
