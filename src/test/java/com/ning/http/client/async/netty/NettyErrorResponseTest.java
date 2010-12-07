a434845d2f13172491ba1e77a8d372eacad81844
jfarcand <jfarcand@apache.org>
2010-12-07 16:10:36 -0500

    Fix refactoring typo

--- src/test/java/com/ning/http/client/async/netty/NettyErrorResponseTest.java
+++ src/test/java/com/ning/http/client/async/netty/NettyErrorResponseTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.ning.http.client.async.jdk;
+package com.ning.http.client.async.netty;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
