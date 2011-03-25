cdcbd453a2cc29e6e1e2b1afb2ae75c5c5c92592
jfarcand <jfarcand@apache.org>
2011-03-25 10:59:02 -0400

    Oups

--- src/test/java/com/ning/http/client/async/netty/NettyBasicAuthTest.java
+++ src/test/java/com/ning/http/client/async/netty/NettyBasicAuthTest.java
@@ -21,7 +21,7 @@ public class NettyBasicAuthTest extends BasicAuthTest {
 
     @Override
     public AsyncHttpClient getAsyncHttpClient(AsyncHttpClientConfig config) {
-        return ProviderUtil.jdkProvider(config);
+        return ProviderUtil.nettyProvider(config);
     }
     
 }
