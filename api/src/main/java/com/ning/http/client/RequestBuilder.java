d482eca72032cf5a41f05c0d8bc654b0deb963c3
Stephane Landelle <slandelle+github@excilys.com>
2013-03-11 12:39:06 +0100

    Mention that RequestBuilder is mutable and not threadsafe, close #238

--- api/src/main/java/com/ning/http/client/RequestBuilder.java
+++ api/src/main/java/com/ning/http/client/RequestBuilder.java
@@ -23,6 +23,8 @@ import java.util.Map;
 
 /**
  * Builder for a {@link Request}.
+ * Warning: mutable and not thread-safe! Beware that it holds a reference on the Request instance it builds,
+ * so modifying the builder will modify the request even after it has been built.
  */
 public class RequestBuilder extends RequestBuilderBase<RequestBuilder> {
 
