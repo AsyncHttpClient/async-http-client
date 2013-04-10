1e00231166f991918685a03af14725db7a8cf011
Ryan Lubke <ryan.lubke@oracle.com>
2013-04-09 22:08:27 -0700

    Add logging.

--- providers/grizzly/src/main/java/com/ning/http/client/providers/grizzly/GSSSPNEGOWrapper.java
+++ providers/grizzly/src/main/java/com/ning/http/client/providers/grizzly/GSSSPNEGOWrapper.java
@@ -7,16 +7,17 @@ import org.ietf.jgss.GSSName;
 import org.ietf.jgss.Oid;
 
 import com.ning.http.util.Base64;
+import org.slf4j.LoggerFactory;
 
 public class GSSSPNEGOWrapper {
+    private final static org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GSSSPNEGOWrapper.class);
+    private static final String KERBEROS_OID = "1.2.840.113554.1.2.2";
 
-	private static final String KERBEROS_OID = "1.2.840.113554.1.2.2";
-	
-	static GSSManager getManager() {
+    static GSSManager getManager() {
         return GSSManager.getInstance();
     }
-	
-	static  byte[] generateGSSToken(
+
+    static byte[] generateGSSToken(
             final byte[] input, final Oid oid, final String authServer) throws GSSException {
         byte[] token = input;
         if (token == null) {
@@ -30,20 +31,18 @@ public class GSSSPNEGOWrapper {
         gssContext.requestCredDeleg(true);
         return gssContext.initSecContext(token, 0, token.length);
     }
-	
-	public  static String generateToken(String authServer)
-	{
-		String  returnVal = "";
-		Oid oid;
-		try {
-			oid = new Oid(KERBEROS_OID);
-			byte[] token = GSSSPNEGOWrapper.generateGSSToken(null, oid, authServer);
-			returnVal = new String(Base64.encode(token));
-		} catch (GSSException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return returnVal;
-	}
+
+    public static String generateToken(String authServer) {
+        String returnVal = "";
+        Oid oid;
+        try {
+            oid = new Oid(KERBEROS_OID);
+            byte[] token = GSSSPNEGOWrapper.generateGSSToken(null, oid, authServer);
+            returnVal = Base64.encode(token);
+        } catch (GSSException e) {
+            LOGGER.warn(e.toString(), e);
+        }
+
+        return returnVal;
+    }
 }
