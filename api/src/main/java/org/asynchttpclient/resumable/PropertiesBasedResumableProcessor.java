1f5bc6bea637838f14f8705d63dae40e1e7216bb
Ryan Lubke <ryan.lubke@oracle.com>
2013-05-28 15:23:50 -0700

    Make sure temp files are deleted upon VM exit.

--- api/src/main/java/org/asynchttpclient/resumable/PropertiesBasedResumableProcessor.java
+++ api/src/main/java/org/asynchttpclient/resumable/PropertiesBasedResumableProcessor.java
@@ -30,6 +30,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class PropertiesBasedResumableProcessor implements ResumableAsyncHandler.ResumableProcessor {
     private final static Logger log = LoggerFactory.getLogger(PropertiesBasedResumableProcessor.class);
     private final static File TMP = new File(System.getProperty("java.io.tmpdir"), "ahc");
+    static {
+        TMP.deleteOnExit();
+    }
     private final static String storeName = "ResumableAsyncHandler.properties";
     private final ConcurrentHashMap<String, Long> properties = new ConcurrentHashMap<String, Long>();
 
@@ -64,6 +67,7 @@ public class PropertiesBasedResumableProcessor implements ResumableAsyncHandler.
                 throw new IllegalStateException("Unable to create directory: " + TMP.getAbsolutePath());
             }
             File f = new File(TMP, storeName);
+            f.deleteOnExit();
             if (!f.createNewFile()) {
                 throw new IllegalStateException("Unable to create temp file: " + f.getAbsolutePath());
             }
@@ -99,7 +103,9 @@ public class PropertiesBasedResumableProcessor implements ResumableAsyncHandler.
     /* @Override */
     public Map<String, Long> load() {
         try {
-            Scanner scan = new Scanner(new File(TMP, storeName), "UTF-8");
+            File f = new File(TMP, storeName);
+            f.deleteOnExit();
+            Scanner scan = new Scanner(f, "UTF-8");
             scan.useDelimiter("[=\n]");
 
             String key;
