3d79612ad17b2fe6236135922fd23164e62f6868
Stephane Landelle <slandelle+github@excilys.com>
2013-07-23 22:32:13 +0200

    Fix NPE

--- src/main/java/com/ning/http/client/providers/netty/NettyConnectionsPool.java
+++ src/main/java/com/ning/http/client/providers/netty/NettyConnectionsPool.java
@@ -60,8 +60,8 @@ public class NettyConnectionsPool implements ConnectionsPool<String, Channel> {
         this.sslConnectionPoolEnabled = sslConnectionPoolEnabled;
         this.maxIdleTime = maxIdleTime;
         this.maxConnectionLifeTimeInMs = maxConnectionLifeTimeInMs;
-        this.idleConnectionDetector.schedule(new IdleChannelDetector(), maxIdleTime, maxIdleTime);
         this.idleConnectionDetector = idleConnectionDetector;
+        this.idleConnectionDetector.schedule(new IdleChannelDetector(), maxIdleTime, maxIdleTime);
     }
 
     private static class IdleChannel {
