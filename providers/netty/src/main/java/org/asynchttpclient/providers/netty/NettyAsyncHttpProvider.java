AsyncHttpClient/async-http-client

commit d9a218ec9be1a4386af722d011df7dc7aa22c826

Don't compute regular host if there's a virtual one

providers/netty/src/main/java/org/asynchttpclient/providers/netty/NettyAsyncHttpProvider.java
@@ -577,12 +577,14 @@ public class NettyAsyncHttpProvider extends SimpleChannelUpstreamHandler impleme
 
     private static HttpRequest construct(AsyncHttpClientConfig config, Request request, HttpMethod m, URI uri, ChannelBuffer buffer, ProxyServer proxyServer) throws IOException {
 
-        String host = AsyncHttpProviderUtils.getHost(uri);
+        String host = null;
         boolean webSocket = isWebSocket(uri);
 
         if (request.getVirtualHost() != null) {
             host = request.getVirtualHost();
-        }
+        } else {
+            AsyncHttpProviderUtils.getHost(uri);
+    	}
 
         HttpRequest nettyRequest;
         if (m.equals(HttpMethod.CONNECT)) {
@@ -607,9 +609,7 @@ public class NettyAsyncHttpProvider extends SimpleChannelUpstreamHandler impleme
         }
 
         if (host != null) {
-            if (uri.getPort() == -1) {
-                nettyRequest.setHeader(HttpHeaders.Names.HOST, host);
-            } else if (request.getVirtualHost() != null) {
+            if (request.getVirtualHost() != null || uri.getPort() == -1) {
                 nettyRequest.setHeader(HttpHeaders.Names.HOST, host);
             } else {
                 nettyRequest.setHeader(HttpHeaders.Names.HOST, host + ":" + uri.getPort());

commit be6a29dd25861c8c8e12dedaa62f63a33898d80e

Fix bug introduced in d9a218ec9be1a4386af722d011df7dc7aa22c826

providers/netty/src/main/java/org/asynchttpclient/providers/netty/NettyAsyncHttpProvider.java
@@ -587,7 +587,7 @@ public class NettyAsyncHttpProvider extends SimpleChannelUpstreamHandler impleme
         if (request.getVirtualHost() != null) {
             host = request.getVirtualHost();
         } else {
-            AsyncHttpProviderUtils.getHost(uri);
+            host = AsyncHttpProviderUtils.getHost(uri);
     	}
 
         HttpRequest nettyRequest;