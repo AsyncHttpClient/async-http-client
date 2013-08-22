adc88eee0c89bd198a81946760b9cbaaab2271d9
Ryan Lubke <ryan.lubke@oracle.com>
2013-08-22 12:37:35 -0700

    Defer creation of composite buffer until the body is needed.

--- providers/grizzly/src/main/java/org/asynchttpclient/providers/grizzly/GrizzlyResponse.java
+++ providers/grizzly/src/main/java/org/asynchttpclient/providers/grizzly/GrizzlyResponse.java
@@ -45,8 +45,12 @@ import java.util.List;
  * @since 1.7.0
  */
 public class GrizzlyResponse extends ResponseBase {
-    private final Buffer responseBody;
+
     private final Boolean rfc6265Enabled;
+
+    private Buffer responseBody;
+    private boolean initialized;
+
 
     // ------------------------------------------------------------ Constructors
 
@@ -57,25 +61,9 @@ public class GrizzlyResponse extends ResponseBase {
                            final boolean rfc6265Enabled) {
         super(status, headers, bodyParts);
         this.rfc6265Enabled = rfc6265Enabled;
-        if (isNonEmpty(bodyParts)) {
-            if (bodyParts.size() == 1) {
-                responseBody = ((GrizzlyResponseBodyPart) bodyParts.get(0)).getBodyBuffer();
-            } else {
-                final Buffer firstBuffer = ((GrizzlyResponseBodyPart) bodyParts.get(0)).getBodyBuffer();
-                final MemoryManager<?> mm = MemoryManager.DEFAULT_MEMORY_MANAGER;
-                Buffer constructedBodyBuffer = firstBuffer;
-                for (int i = 1, len = bodyParts.size(); i < len; i++) {
-                    constructedBodyBuffer =
-                            Buffers.appendBuffers(mm,
-                                    constructedBodyBuffer,
-                                    ((GrizzlyResponseBodyPart) bodyParts.get(i)).getBodyBuffer());
-                }
-                responseBody = constructedBodyBuffer;
-            }
-        } else {
-            responseBody = Buffers.EMPTY_BUFFER;
-        }
     }
+
+
 
 
     // --------------------------------------------------- Methods from Response
@@ -85,7 +73,7 @@ public class GrizzlyResponse extends ResponseBase {
      */
     public InputStream getResponseBodyAsStream() throws IOException {
 
-        return new BufferInputStream(responseBody);
+        return new BufferInputStream(getResponseBody0());
 
     }
 
@@ -95,6 +83,7 @@ public class GrizzlyResponse extends ResponseBase {
      */
     public String getResponseBodyExcerpt(int maxLength, String charset) throws IOException {
        charset = calculateCharset(charset);
+        final Buffer responseBody = getResponseBody0();
         final int len = Math.min(responseBody.remaining(), maxLength);
         final int pos = responseBody.position();
         return responseBody.toStringContent(getCharset(charset), pos, len + pos);
@@ -107,7 +96,7 @@ public class GrizzlyResponse extends ResponseBase {
      */
     public String getResponseBody(String charset) throws IOException {
 
-        return responseBody.toStringContent(getCharset(charset));
+        return getResponseBody0().toStringContent(getCharset(charset));
 
     }
 
@@ -149,7 +138,7 @@ public class GrizzlyResponse extends ResponseBase {
      */
     @SuppressWarnings("UnusedDeclaration")
     public Buffer getResponseBodyAsBuffer() {
-        return responseBody;
+        return getResponseBody0();
     }
 
     /**
@@ -214,5 +203,35 @@ public class GrizzlyResponse extends ResponseBase {
 
         return Charsets.lookupCharset(charsetLocal);
 
+    }
+
+    private synchronized Buffer getResponseBody0() {
+        if (!initialized) {
+            if (isNonEmpty(bodyParts)) {
+                if (bodyParts.size() == 1) {
+                    responseBody = ((GrizzlyResponseBodyPart) bodyParts.get(
+                            0)).getBodyBuffer();
+                } else {
+                    final Buffer firstBuffer =
+                            ((GrizzlyResponseBodyPart) bodyParts.get(
+                                    0)).getBodyBuffer();
+                    final MemoryManager<?> mm =
+                            MemoryManager.DEFAULT_MEMORY_MANAGER;
+                    Buffer constructedBodyBuffer = firstBuffer;
+                    for (int i = 1, len = bodyParts.size(); i < len; i++) {
+                        constructedBodyBuffer =
+                                Buffers.appendBuffers(mm,
+                                                      constructedBodyBuffer,
+                                                      ((GrizzlyResponseBodyPart) bodyParts
+                                                              .get(i)).getBodyBuffer());
+                    }
+                    responseBody = constructedBodyBuffer;
+                }
+            } else {
+                responseBody = Buffers.EMPTY_BUFFER;
+            }
+            initialized = true;
+        }
+        return responseBody;
     }
 }
