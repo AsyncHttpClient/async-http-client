93e5ad90dbb02804c0e82ad9841fb8087efce695
Stephane Landelle <slandelle@excilys.com>
2013-09-05 13:33:53 +0200

    Properly initialize AsyncHttpClientConfigBean.ioThreadMultiplier
    Otherwise, Netty provider will crash as worker count will be 0

--- src/main/java/com/ning/http/client/AsyncHttpClientConfigBean.java
+++ src/main/java/com/ning/http/client/AsyncHttpClientConfigBean.java
@@ -54,6 +54,7 @@ public class AsyncHttpClientConfigBean extends AsyncHttpClientConfig {
         maxDefaultRedirects = Integer.getInteger(ASYNC_CLIENT + "defaultMaxRedirects", 5);
         compressionEnabled = Boolean.getBoolean(ASYNC_CLIENT + "compressionEnabled");
         userAgent = System.getProperty(ASYNC_CLIENT + "userAgent", "NING/1.0");
+        ioThreadMultiplier = Integer.getInteger(ASYNC_CLIENT + "ioThreadMultiplier", 8);
 
         boolean useProxySelector = Boolean.getBoolean(ASYNC_CLIENT + "useProxySelector");
         boolean useProxyProperties = Boolean.getBoolean(ASYNC_CLIENT + "useProxyProperties");
