3f5a2b21c1d2c66d07ec0cc2509e2e02c66baaaf
Stephane Landelle <slandelle@excilys.com>
2014-02-18 11:06:48 +0100

    Add a test about the server replying 401 while the client uploads a big file

---
+++ api/src/test/java/org/asynchttpclient/async/FastUnauthorizedUploadTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2010-2012 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.asynchttpclient.async;
+
+import static org.asynchttpclient.async.util.TestUtils.createTempFile;
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Response;
+import org.asynchttpclient.multipart.FilePart;
+import org.asynchttpclient.util.StandardCharsets;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.testng.annotations.Test;
+
+public abstract class FastUnauthorizedUploadTest extends AbstractBasicTest {
+
+    @Override
+    public AbstractHandler configureHandler() throws Exception {
+        return new AbstractHandler() {
+
+            public void handle(String target, Request baseRequest, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+                resp.setStatus(401);
+                resp.getOutputStream().flush();
+                resp.getOutputStream().close();
+
+                baseRequest.setHandled(true);
+            }
+        };
+    }
+
+    @Test(groups = { "standalone", "default_provider" }, enabled = true)
+    public void testUnauthorizedWhileUploading() throws Exception {
+        File file = createTempFile(1024 * 1024);
+
+        AsyncHttpClient client = getAsyncHttpClient(null);
+        try {
+            Response response = client.preparePut(getTargetUrl()).addBodyPart(new FilePart("test", file, "application/octet-stream", StandardCharsets.UTF_8.name())).execute()
+                    .get();
+            assertEquals(response.getStatusCode(), 401);
+        } finally {
+            client.close();
+        }
+    }
+}
