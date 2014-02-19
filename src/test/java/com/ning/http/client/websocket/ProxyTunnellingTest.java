dde2652255a945d65e1edb9da663924c3715c0bd
Stephane Landelle <slandelle@excilys.com>
2014-02-19 17:23:09 +0100

    Backport test for wss proxy tunneling: both providers are fine on this branch

---
+++ src/test/java/com/ning/http/client/websocket/ProxyTunnellingTest.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2014 AsyncHttpClient Project. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.ning.http.client.websocket;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ProxyHandler;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.ProxyServer;
+import com.ning.http.client.websocket.TextMessageTest.EchoTextWebSocket;
+
+/**
+ * Proxy usage tests.
+ */
+public abstract class ProxyTunnellingTest extends AbstractBasicTest {
+
+    int port2;
+    private Server server2;
+
+    public AbstractHandler configureHandler() throws Exception {
+        ProxyHandler proxy = new ProxyHandler();
+        return proxy;
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpGlobal() throws Exception {
+        server2 = new Server();
+
+        port1 = findFreePort();
+        port2 = findFreePort();
+
+        Connector listener = new SelectChannelConnector();
+
+        listener.setHost("127.0.0.1");
+        listener.setPort(port1);
+
+        addConnector(listener);
+
+        SslSelectChannelConnector connector = new SslSelectChannelConnector();
+        connector.setHost("127.0.0.1");
+        connector.setPort(port2);
+
+        ClassLoader cl = getClass().getClassLoader();
+        URL keystoreUrl = cl.getResource("ssltest-keystore.jks");
+        String keyStoreFile = new File(keystoreUrl.toURI()).getAbsolutePath();
+        connector.setKeystore(keyStoreFile);
+        connector.setKeyPassword("changeit");
+        connector.setKeystoreType("JKS");
+
+        server2.addConnector(connector);
+
+        setHandler(configureHandler());
+        start();
+
+        server2.setHandler(getWebSocketHandler());
+        server2.start();
+        log.info("Local HTTP server started successfully");
+    }
+
+    @Override
+    public WebSocketHandler getWebSocketHandler() {
+        return new WebSocketHandler() {
+            @Override
+            public org.eclipse.jetty.websocket.WebSocket doWebSocketConnect(HttpServletRequest httpServletRequest, String s) {
+                return new EchoTextWebSocket();
+            }
+        };
+    }
+    
+    @AfterClass(alwaysRun = true)
+    public void tearDownGlobal() throws Exception {
+        stop();
+        server2.stop();
+    }
+
+    protected String getTargetUrl() {
+        return String.format("wss://127.0.0.1:%d/", port2);
+    }
+
+    @Test(timeOut = 60000)
+    public void echoText() throws Exception {
+
+        ProxyServer ps = new ProxyServer(ProxyServer.Protocol.HTTPS, "127.0.0.1", port1);
+        AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder().setProxyServer(ps).build();
+        AsyncHttpClient asyncHttpClient = getAsyncHttpClient(config);
+        try {
+            final CountDownLatch latch = new CountDownLatch(1);
+            final AtomicReference<String> text = new AtomicReference<String>("");
+
+            WebSocket websocket = asyncHttpClient.prepareGet(getTargetUrl()).execute(new WebSocketUpgradeHandler.Builder().addWebSocketListener(new WebSocketTextListener() {
+
+                @Override
+                public void onMessage(String message) {
+                    text.set(message);
+                    latch.countDown();
+                }
+
+                @Override
+                public void onFragment(String fragment, boolean last) {
+                }
+
+                @Override
+                public void onOpen(WebSocket websocket) {
+                }
+
+                @Override
+                public void onClose(WebSocket websocket) {
+                    latch.countDown();
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    t.printStackTrace();
+                    latch.countDown();
+                }
+            }).build()).get();
+
+            websocket.sendTextMessage("ECHO");
+
+            latch.await();
+            assertEquals(text.get(), "ECHO");
+        } finally {
+            asyncHttpClient.close();
+        }
+    }
+}
