a5c75a4161e1ee4db6b3daceece950336d6561a6
Stephane Landelle <slandelle@excilys.com>
2014-08-26 15:17:14 +0200

    UriComponent doesn't properly handle ./ relative urls, close #680

--- api/src/main/java/org/asynchttpclient/uri/UriComponentsParser.java
+++ api/src/main/java/org/asynchttpclient/uri/UriComponentsParser.java
@@ -200,7 +200,7 @@ final class UriComponentsParser {
 
     // /./
     private void removeEmbeddedDot() {
-        path = path.replace("/./", "");
+        path = path.replace("/./", "/");
     }
 
     // /../
