f5736c101ecb8aa2679d0e29520945cb2bfb4cd1
Gerd Riesselmann <gerd@gerd-riesselmann.net>
2014-09-02 13:21:21 +0200

    Java 8 Compatibility Fixes

--- src/test/java/com/ning/http/client/async/FluentCaseInsensitiveStringsMapTest.java
+++ src/test/java/com/ning/http/client/async/FluentCaseInsensitiveStringsMapTest.java
@@ -391,7 +391,7 @@ public class FluentCaseInsensitiveStringsMapTest {
         assertEquals(map.getJoinedValue("baz", ", "), "foo, bar");
         assertEquals(map.get("baz"), Arrays.asList("foo", "bar"));
 
-        map.replace("Foo", "blub", "bla");
+        map.replaceWith("Foo", "blub", "bla");
 
         assertEquals(map.keySet(), new LinkedHashSet<String>(Arrays.asList("Foo", "baz")));
         assertEquals(map.getFirstValue("foo"), "blub");
@@ -417,7 +417,7 @@ public class FluentCaseInsensitiveStringsMapTest {
         assertEquals(map.getJoinedValue("baz", ", "), "foo, bar");
         assertEquals(map.get("baz"), Arrays.asList("foo", "bar"));
 
-        map.replace("bar", Arrays.asList("blub"));
+        map.replaceWith("bar", Arrays.asList("blub"));
 
         assertEquals(map.keySet(), new LinkedHashSet<String>(Arrays.asList("foo", "baz", "bar")));
         assertEquals(map.getFirstValue("foo"), "bar");
@@ -446,7 +446,7 @@ public class FluentCaseInsensitiveStringsMapTest {
         assertEquals(map.getJoinedValue("baz", ", "), "foo, bar");
         assertEquals(map.get("baz"), Arrays.asList("foo", "bar"));
 
-        map.replace(null, Arrays.asList("blub"));
+        map.replaceWith(null, Arrays.asList("blub"));
 
         assertEquals(map.keySet(), new LinkedHashSet<String>(Arrays.asList("foo", "baz")));
         assertEquals(map.getFirstValue("foo"), "bar");
@@ -472,7 +472,7 @@ public class FluentCaseInsensitiveStringsMapTest {
         assertEquals(map.getJoinedValue("baz", ", "), "foo, bar");
         assertEquals(map.get("baz"), Arrays.asList("foo", "bar"));
 
-        map.replace("baZ", (Collection<String>) null);
+        map.replaceWith("baZ", (Collection<String>) null);
 
         assertEquals(map.keySet(), new LinkedHashSet<String>(Arrays.asList("foo")));
         assertEquals(map.getFirstValue("foo"), "bar");
