01b33d3f8cd0d8a85e1d3c29fe7fa56367c8ecf6
Stephane Landelle <slandelle@excilys.com>
2014-09-15 10:44:29 +0200

    Clean up canRetry

--- providers/netty/src/main/java/org/asynchttpclient/providers/netty/future/NettyResponseFuture.java
+++ providers/netty/src/main/java/org/asynchttpclient/providers/netty/future/NettyResponseFuture.java
@@ -420,10 +420,7 @@ public final class NettyResponseFuture<V> extends AbstractListenableFuture<V> {
     }
 
     public boolean canRetry() {
-        if (currentRetry.incrementAndGet() > maxRetry) {
-            return false;
-        }
-        return true;
+        return maxRetry >= 0 && currentRetry.incrementAndGet() <= maxRetry;
     }
 
     public SocketAddress getChannelRemoteAddress() {
