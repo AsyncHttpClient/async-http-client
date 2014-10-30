01031b6132fc7b671354f46c94cf94b5b777898a
Stephane Landelle <slandelle@excilys.com>
2014-10-30 15:03:59 +0100

    Made logger static to improve performance. Fixed logger name, backport #749

--- src/main/java/com/ning/http/client/AsyncCompletionHandler.java
+++ src/main/java/com/ning/http/client/AsyncCompletionHandler.java
@@ -28,7 +28,7 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class AsyncCompletionHandler<T> implements AsyncHandler<T>, ProgressAsyncHandler<T> {
 
-    private final Logger log = LoggerFactory.getLogger(AsyncCompletionHandlerBase.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AsyncCompletionHandler.class);
     private final Response.ResponseBuilder builder = new Response.ResponseBuilder();
 
     @Override
@@ -57,7 +57,7 @@ public abstract class AsyncCompletionHandler<T> implements AsyncHandler<T>, Prog
 
     @Override
     public void onThrowable(Throwable t) {
-        log.debug(t.getMessage(), t);
+        LOGGER.debug(t.getMessage(), t);
     }
 
     /**
