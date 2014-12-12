0f514b1ecb25b095ae95568a69278e7ffa0ff7e0
Stephane Landelle <slandelle@excilys.com>
2014-12-12 12:19:19 +0100

    Fix tests

--- api/src/test/java/org/asynchttpclient/async/RemoteSiteTest.java
+++ api/src/test/java/org/asynchttpclient/async/RemoteSiteTest.java
@@ -195,23 +195,7 @@ public abstract class RemoteSiteTest extends AbstractBasicTest {
             String requestUrl2 = URL + URLEncoder.encode(REQUEST_PARAM, UTF_8.name());
             logger.info(String.format("Executing request [%s] ...", requestUrl2));
             Response response = client.prepareGet(requestUrl2).execute().get();
-            assertEquals(response.getStatusCode(), 301);
-        } finally {
-            client.close();
-        }
-    }
-
-    /**
-     * See https://issues.sonatype.org/browse/AHC-61
-     * 
-     * @throws Exception
-     */
-    @Test(groups = { "online", "default_provider" })
-    public void testAHC60() throws Exception {
-        AsyncHttpClient client = getAsyncHttpClient(null);
-        try {
-            Response response = client.prepareGet("http://www.meetup.com/stackoverflow/Mountain-View-CA/").execute().get();
-            assertEquals(response.getStatusCode(), 200);
+            assertEquals(response.getStatusCode(), 302);
         } finally {
             client.close();
         }
