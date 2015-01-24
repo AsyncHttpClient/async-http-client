d7f9662c444b8542a4318b65a88ab504bd6f3462
Stephane Landelle <slandelle@excilys.com>
2015-01-24 13:55:11 +0100

    Set closeOnSSLException flag to true, close #807

--- src/main/java/com/ning/http/client/providers/netty/channel/ChannelManager.java
+++ src/main/java/com/ning/http/client/providers/netty/channel/ChannelManager.java
@@ -394,8 +394,10 @@ public class ChannelManager {
 
     public SslHandler createSslHandler(String peerHost, int peerPort) throws GeneralSecurityException, IOException {
         SSLEngine sslEngine = sslEngineFactory.newSSLEngine(peerHost, peerPort);
-        return handshakeTimeout > 0 ? new SslHandler(sslEngine, getDefaultBufferPool(), false, nettyTimer, handshakeTimeout)
-                : new SslHandler(sslEngine);
+        SslHandler sslHandler = handshakeTimeout > 0 ? new SslHandler(sslEngine, getDefaultBufferPool(), false, nettyTimer,
+                handshakeTimeout) : new SslHandler(sslEngine);
+        sslHandler.setCloseOnSSLException(true);
+        return sslHandler;
     }
 
     public static SslHandler getSslHandler(ChannelPipeline pipeline) {
