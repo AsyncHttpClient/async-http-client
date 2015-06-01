cf247653af9ff0fe46f02b177f0957b2e452dd9f
Stephane Landelle <slandelle@gatling.io>
2015-06-02 00:18:25 +0200

    Fix NPE on partitionKey when maxConnectionsPerHost is enabled, close #900

--- src/main/java/com/ning/http/client/providers/netty/request/NettyRequestSender.java
+++ src/main/java/com/ning/http/client/providers/netty/request/NettyRequestSender.java
@@ -266,15 +266,11 @@ public final class NettyRequestSender {
         ClientBootstrap bootstrap = channelManager.getBootstrap(request.getUri().getScheme(), useProxy, useSSl);
 
         boolean channelPreempted = false;
-        Object partitionKey = null;
+        Object partitionKey = future.getPartitionKey();
 
         try {
             // Do not throw an exception when we need an extra connection for a redirect.
             if (!reclaimCache) {
-                // only compute when maxConnectionPerHost is enabled
-                // FIXME clean up
-                if (config.getMaxConnectionsPerHost() > 0)
-                    partitionKey = future.getPartitionKey();
                 channelManager.preemptChannel(partitionKey);
                 channelPreempted = true;
             }
