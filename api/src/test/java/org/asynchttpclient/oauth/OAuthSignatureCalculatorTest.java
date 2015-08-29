2f02fdd0ac7e7b04a6756132bdcf7afb9b225de9
Stephane Landelle <slandelle@gatling.io>
2015-08-29 08:12:24 +0200

    Prove #960 isn't an AHC bug, close #960

--- api/src/test/java/org/asynchttpclient/oauth/OAuthSignatureCalculatorTest.java
+++ api/src/test/java/org/asynchttpclient/oauth/OAuthSignatureCalculatorTest.java
@@ -258,7 +258,43 @@ public class OAuthSignatureCalculatorTest {
         }
 
         assertEquals(sig, "tR3+Ty81lMeYAr/Fid0kMTYa/WM=");
-
+        assertEquals(req.getUrl(), "http://photos.example.net/photos?file=vacation.jpg&size=original");
     }
 
+    @Test(groups = "fast")
+    public void testGetWithRequestBuilderAndQuery() {
+        ConsumerKey consumer = new ConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
+        RequestToken user = new RequestToken(TOKEN_KEY, TOKEN_SECRET);
+        OAuthSignatureCalculator calc = new StaticOAuthSignatureCalculator(consumer, user, TIMESTAMP, NONCE);
+
+        String url = "http://photos.example.net/photos?file=vacation.jpg&size=original";
+
+        final Request req = new RequestBuilder("GET")//
+                .setUri(Uri.create(url))//
+                .setSignatureCalculator(calc)//
+                .build();
+
+        final List<Param> params = req.getQueryParams();
+        assertEquals(params.size(), 2);
+
+        // From the signature tester, the URL should look like:
+        //normalized parameters: file=vacation.jpg&oauth_consumer_key=dpf43f3p2l4k3l03&oauth_nonce=kllo9940pd9333jh&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1191242096&oauth_token=nnch734d00sl2jdk&oauth_version=1.0&size=original
+        //signature base string: GET&http%3A%2F%2Fphotos.example.net%2Fphotos&file%3Dvacation.jpg%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3Dnnch734d00sl2jdk%26oauth_version%3D1.0%26size%3Doriginal
+        //signature: tR3+Ty81lMeYAr/Fid0kMTYa/WM=
+        //Authorization header: OAuth realm="",oauth_version="1.0",oauth_consumer_key="dpf43f3p2l4k3l03",oauth_token="nnch734d00sl2jdk",oauth_timestamp="1191242096",oauth_nonce="kllo9940pd9333jh",oauth_signature_method="HMAC-SHA1",oauth_signature="tR3%2BTy81lMeYAr%2FFid0kMTYa%2FWM%3D"
+
+        String authHeader = req.getHeaders().get("Authorization").get(0);
+        Matcher m = Pattern.compile("oauth_signature=\"(.+?)\"").matcher(authHeader);
+        assertEquals(m.find(), true);
+        String encodedSig = m.group(1);
+        String sig = null;
+        try {
+            sig = URLDecoder.decode(encodedSig, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            fail("bad encoding", e);
+        }
+
+        assertEquals(sig, "tR3+Ty81lMeYAr/Fid0kMTYa/WM=");
+        assertEquals(req.getUrl(), "http://photos.example.net/photos?file=vacation.jpg&size=original");
+    }
 }
