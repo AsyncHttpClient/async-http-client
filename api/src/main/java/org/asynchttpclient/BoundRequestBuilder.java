e8060d166b4581be25deccd8f5474ee47dbfc1d0
Stephane Landelle <slandelle@gatling.io>
2015-09-02 11:07:35 +0200

    Clojure issues are Clojure's concern!

--- api/src/main/java/org/asynchttpclient/BoundRequestBuilder.java
+++ api/src/main/java/org/asynchttpclient/BoundRequestBuilder.java
@@ -12,13 +12,6 @@
  */
 package org.asynchttpclient;
 
-import org.asynchttpclient.cookie.Cookie;
-import org.asynchttpclient.request.body.multipart.Part;
-
-import java.io.InputStream;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 public class BoundRequestBuilder extends RequestBuilderBase<BoundRequestBuilder> {
 
@@ -40,94 +33,5 @@ public class BoundRequestBuilder extends RequestBuilderBase<BoundRequestBuilder>
 
     public ListenableFuture<Response> execute() {
         return client.executeRequest(build(), new AsyncCompletionHandlerBase());
-    }
-
-    // Note: For now we keep the delegates in place even though they are not
-    // needed
-    // since otherwise Clojure (and maybe other languages) won't be able to
-    // access these methods - see Clojure tickets 126 and 259
-
-    @Override
-    public BoundRequestBuilder addBodyPart(Part part) {
-        return super.addBodyPart(part);
-    }
-
-    @Override
-    public BoundRequestBuilder addCookie(Cookie cookie) {
-        return super.addCookie(cookie);
-    }
-
-    @Override
-    public BoundRequestBuilder addHeader(String name, String value) {
-        return super.addHeader(name, value);
-    }
-
-    @Override
-    public BoundRequestBuilder addFormParam(String key, String value) {
-        return super.addFormParam(key, value);
-    }
-
-    @Override
-    public BoundRequestBuilder addQueryParam(String name, String value) {
-        return super.addQueryParam(name, value);
-    }
-
-    @Override
-    public Request build() {
-        return super.build();
-    }
-
-    @Override
-    public BoundRequestBuilder setBody(byte[] data) {
-        return super.setBody(data);
-    }
-
-    @Override
-    public BoundRequestBuilder setBody(InputStream stream) {
-        return super.setBody(stream);
-    }
-
-    @Override
-    public BoundRequestBuilder setBody(String data) {
-        return super.setBody(data);
-    }
-
-    @Override
-    public BoundRequestBuilder setHeader(String name, String value) {
-        return super.setHeader(name, value);
-    }
-
-    @Override
-    public BoundRequestBuilder setHeaders(FluentCaseInsensitiveStringsMap headers) {
-        return super.setHeaders(headers);
-    }
-
-    @Override
-    public BoundRequestBuilder setHeaders(Map<String, Collection<String>> headers) {
-        return super.setHeaders(headers);
-    }
-
-    @Override
-    public BoundRequestBuilder setFormParams(Map<String, List<String>> params) {
-        return super.setFormParams(params);
-    }
-
-    @Override
-    public BoundRequestBuilder setFormParams(List<Param> params) {
-        return super.setFormParams(params);
-    }
-
-    @Override
-    public BoundRequestBuilder setUrl(String url) {
-        return super.setUrl(url);
-    }
-
-    @Override
-    public BoundRequestBuilder setVirtualHost(String virtualHost) {
-        return super.setVirtualHost(virtualHost);
-    }
-
-    public BoundRequestBuilder setSignatureCalculator(SignatureCalculator signatureCalculator) {
-        return super.setSignatureCalculator(signatureCalculator);
     }
 }
