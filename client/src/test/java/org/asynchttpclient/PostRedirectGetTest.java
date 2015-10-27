301497f8e774e00997067a461d81802623a25fae
Stephane Landelle <slandelle@gatling.io>
2015-10-27 14:20:52 +0100

    Fix PostRedirectGetTest, close #761

--- client/src/test/java/org/asynchttpclient/PostRedirectGetTest.java
+++ client/src/test/java/org/asynchttpclient/PostRedirectGetTest.java
@@ -41,27 +41,27 @@ public class PostRedirectGetTest extends AbstractBasicTest {
 
     // ------------------------------------------------------------ Test Methods
 
-    @Test(groups = { "standalone", "post_redirect_get" }, enabled = false)
+    @Test(groups = { "standalone", "post_redirect_get" })
     public void postRedirectGet302Test() throws Exception {
         doTestPositive(302);
     }
 
-    @Test(groups = { "standalone", "post_redirect_get" }, enabled = false)
+    @Test(groups = { "standalone", "post_redirect_get" })
     public void postRedirectGet302StrictTest() throws Exception {
         doTestNegative(302, true);
     }
 
-    @Test(groups = { "standalone", "post_redirect_get" }, enabled = false)
+    @Test(groups = { "standalone", "post_redirect_get" })
     public void postRedirectGet303Test() throws Exception {
         doTestPositive(303);
     }
 
-    @Test(groups = { "standalone", "post_redirect_get" }, enabled = false)
+    @Test(groups = { "standalone", "post_redirect_get" })
     public void postRedirectGet301Test() throws Exception {
-        doTestNegative(301, false);
+        doTestPositive(301);
     }
 
-    @Test(groups = { "standalone", "post_redirect_get" }, enabled = false)
+    @Test(groups = { "standalone", "post_redirect_get" })
     public void postRedirectGet307Test() throws Exception {
         doTestNegative(307, false);
     }
