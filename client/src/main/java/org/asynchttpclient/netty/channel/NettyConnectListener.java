10a538c34464b5c28083cc4e221e0598ee4505e0
Stephane Landelle <slandelle@gatling.io>
2016-04-06 09:12:48 +0200

    Forcefully close connection when Future is manually cancelled during connect, close #1131

--- client/src/main/java/org/asynchttpclient/netty/channel/NettyConnectListener.java
+++ client/src/main/java/org/asynchttpclient/netty/channel/NettyConnectListener.java
@@ -69,6 +69,7 @@ public final class NettyConnectListener<T> extends SimpleChannelFutureListener {
 
         if (future.isDone()) {
             abortChannelPreemption();
+            Channels.silentlyCloseChannel(channel);
             return;
         }
 
