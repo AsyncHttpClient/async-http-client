464e20480e8c188d6dcae7dbce2b9c5098d3584f
Stephane Landelle <slandelle@gatling.io>
2016-06-03 07:27:22 +0200

    Fix assertion: this is TestNG, not JUnit

--- client/src/test/java/org/asynchttpclient/ListenableFutureTest.java
+++ client/src/test/java/org/asynchttpclient/ListenableFutureTest.java
@@ -60,7 +60,7 @@ public class ListenableFutureTest extends AbstractBasicTest {
             future.get();
             future.addListener(() -> counter.decrementAndGet(), Runnable::run);
         }
-        assertEquals(0, counter.get());
+        assertEquals(counter.get(), 0);
     }
 
     @Test(groups = "standalone")
@@ -76,6 +76,6 @@ public class ListenableFutureTest extends AbstractBasicTest {
             future.get();
             future.addListener(() -> counter.decrementAndGet(), Runnable::run);
         }
-        assertEquals(0, counter.get());
+        assertEquals(counter.get(), 0);
     }
 }
