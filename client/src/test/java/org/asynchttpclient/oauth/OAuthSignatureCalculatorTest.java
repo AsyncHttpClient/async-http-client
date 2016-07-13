3b05d39b8a27f0594d67ddd962bc5ac3c3a5fe42
Stephane Landelle <slandelle@gatling.io>
2016-07-13 16:13:54 +0200

    Clean license header

--- client/src/test/java/org/asynchttpclient/oauth/OAuthSignatureCalculatorTest.java
+++ client/src/test/java/org/asynchttpclient/oauth/OAuthSignatureCalculatorTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 Ning, Inc.
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * This program is licensed to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
