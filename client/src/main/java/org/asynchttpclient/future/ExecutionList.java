8f6d2a2e20d5aa8dd4d2a6b7df5487af4a812fc3
Stepan Koltsov <stepan.koltsov@gmail.com>
2016-10-26 04:25:55 +0300

    Allow null executor in ListenableFuture
    It allows execution of future callback in completion thread, and
    makes `ListenableFuture` closer to `CompletableFuture` (which allows
    `null` executor).
    
    It also saves a little memory in `toCompletableFuture` implementation.

--- client/src/main/java/org/asynchttpclient/future/ExecutionList.java
+++ client/src/main/java/org/asynchttpclient/future/ExecutionList.java
@@ -56,7 +56,6 @@ public final class ExecutionList {
         // Fail fast on a null. We throw NPE here because the contract of Executor states that it
         // throws NPE on null listener, so we propagate that contract up into the add method as well.
         assertNotNull(runnable, "runnable");
-        assertNotNull(executor, "executor");
 
         // Lock while we check state. We must maintain the lock while adding the new pair so that
         // another thread can't run the list out from under us. We only add to the list if we have not
@@ -122,7 +121,11 @@ public final class ExecutionList {
      */
     static void executeListener(Runnable runnable, Executor executor) {
         try {
-            executor.execute(runnable);
+            if (executor != null) {
+                executor.execute(runnable);
+            } else {
+                runnable.run();
+            }
         } catch (RuntimeException e) {
             // Log it and keep going, bad runnable and/or executor. Don't punish the other runnables if
             // we're given a bad one. We only catch RuntimeException because we want Errors to propagate
