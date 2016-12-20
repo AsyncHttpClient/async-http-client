e21781b264a8bbef70ded41ed6cbdddc04912496
Stephane Landelle <slandelle@gatling.io>
2016-12-20 23:44:46 +0100

    Suppress unchecked warning

--- client/src/main/java/org/asynchttpclient/filter/ReleasePermitOnComplete.java
+++ client/src/main/java/org/asynchttpclient/filter/ReleasePermitOnComplete.java
@@ -15,36 +15,38 @@ import org.asynchttpclient.AsyncHandler;
  * This is done via a dynamic proxy to preserve all interfaces of the wrapped handler.
  */
 public class ReleasePermitOnComplete {
-   /**
-    * Wrap handler to release the permit of the semaphore on {@link AsyncHandler#onCompleted()}.
-    */
-   public static <T> AsyncHandler<T> wrap(final AsyncHandler<T> handler, final Semaphore available) {
-      Class<?> handlerClass = handler.getClass();
-      ClassLoader classLoader = handlerClass.getClassLoader();
-      Class<?>[] interfaces = allInterfaces(handlerClass);
 
-      return (AsyncHandler<T>) Proxy.newProxyInstance(classLoader, interfaces, new InvocationHandler() {
-         @Override
-         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            try {
-               return method.invoke(handler, args);
-            } finally {
-               if ("onCompleted".equals(method.getName())) {
-                  available.release();
-               }
+    /**
+     * Wrap handler to release the permit of the semaphore on {@link AsyncHandler#onCompleted()}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> AsyncHandler<T> wrap(final AsyncHandler<T> handler, final Semaphore available) {
+        Class<?> handlerClass = handler.getClass();
+        ClassLoader classLoader = handlerClass.getClassLoader();
+        Class<?>[] interfaces = allInterfaces(handlerClass);
+
+        return (AsyncHandler<T>) Proxy.newProxyInstance(classLoader, interfaces, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                try {
+                    return method.invoke(handler, args);
+                } finally {
+                    if ("onCompleted".equals(method.getName())) {
+                        available.release();
+                    }
+                }
             }
-         }
-      });
-   }
+        });
+    }
 
-   /**
-    * Extract all interfaces of a class.
-    */
-   static Class<?>[] allInterfaces(Class<?> handlerClass) {
-      Set<Class<?>> allInterfaces = new HashSet<>();
-      for (Class<?> clazz = handlerClass; clazz != null; clazz = clazz.getSuperclass()) {
-         Collections.addAll(allInterfaces, clazz.getInterfaces());
-      }
-      return allInterfaces.toArray(new Class[allInterfaces.size()]);
-   }
+    /**
+     * Extract all interfaces of a class.
+     */
+    static Class<?>[] allInterfaces(Class<?> handlerClass) {
+        Set<Class<?>> allInterfaces = new HashSet<>();
+        for (Class<?> clazz = handlerClass; clazz != null; clazz = clazz.getSuperclass()) {
+            Collections.addAll(allInterfaces, clazz.getInterfaces());
+        }
+        return allInterfaces.toArray(new Class[allInterfaces.size()]);
+    }
 }
