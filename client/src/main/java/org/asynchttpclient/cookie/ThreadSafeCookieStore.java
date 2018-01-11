ab63ed95db1b4da51dc8599bafd7cff482e11bad
unoexperto <github@walkmind.com>
2018-01-11 21:11:16 +0300

    Minor optimizations

--- client/src/main/java/org/asynchttpclient/cookie/ThreadSafeCookieStore.java
+++ client/src/main/java/org/asynchttpclient/cookie/ThreadSafeCookieStore.java
@@ -16,12 +16,14 @@ package org.asynchttpclient.cookie;
 
 import io.netty.handler.codec.http.cookie.Cookie;
 import org.asynchttpclient.uri.Uri;
+import org.asynchttpclient.util.Assertions;
+import org.asynchttpclient.util.MiscUtils;
 
 import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
-public class ThreadSafeCookieStore implements CookieStore {
+public final class ThreadSafeCookieStore implements CookieStore {
 
     private Map<CookieKey, StoredCookie> cookieJar = new HashMap<>();
     private ReentrantLock lock = new ReentrantLock();
@@ -56,7 +58,7 @@ public class ThreadSafeCookieStore implements CookieStore {
             final boolean[] removeExpired = {false};
             List<Cookie> result = cookieJar
                     .entrySet()
-                    .parallelStream()
+                    .stream()
                     .filter(pair -> {
                         boolean hasCookieExpired = hasCookieExpired(pair.getValue().cookie, pair.getValue().createdAt);
                         if (hasCookieExpired && !removeExpired[0])
@@ -104,31 +106,29 @@ public class ThreadSafeCookieStore implements CookieStore {
     }
 
     private String requestPath(Uri requestUri) {
-        if (!requestUri.getPath().isEmpty())
-            return requestUri.getPath();
-        else
-            return "/";
+        return requestUri.getPath().isEmpty() ? "/" : requestUri.getPath();
     }
 
     // rfc6265#section-5.2.3
     // Let cookie-domain be the attribute-value without the leading %x2E (".") character.
     private AbstractMap.SimpleEntry<String, Boolean> cookieDomain(String cookieDomain, String requestDomain) {
         if (cookieDomain != null) {
-            if (!cookieDomain.isEmpty() && cookieDomain.charAt(0) == '.')
-                return new AbstractMap.SimpleEntry<>(cookieDomain.substring(1).toLowerCase(), false);
-            else
-                return new AbstractMap.SimpleEntry<>(cookieDomain.toLowerCase(), false);
+            String normalizedCookieDomain = cookieDomain.toLowerCase();
+            return new AbstractMap.SimpleEntry<>(
+                    (!cookieDomain.isEmpty() && cookieDomain.charAt(0) == '.') ?
+                            normalizedCookieDomain.substring(1) :
+                            normalizedCookieDomain, false);
         } else
             return new AbstractMap.SimpleEntry<>(requestDomain, true);
     }
 
     // rfc6265#section-5.2.4
     private String cookiePath(String rawCookiePath, String requestPath) {
-        if (rawCookiePath != null && !rawCookiePath.isEmpty() && rawCookiePath.charAt(0) == '/') {
+        if (MiscUtils.isNonEmpty(rawCookiePath) && rawCookiePath.charAt(0) == '/') {
             return rawCookiePath;
         } else {
             // rfc6265#section-5.1.4
-            if (!requestPath.isEmpty() && requestPath.charAt(0) == '/' && requestPath.chars().filter(value -> value == '/').count() > 1)
+            if (!requestPath.isEmpty() && requestPath.charAt(0) == '/' && requestPath.lastIndexOf('/') > 0)
                 return requestPath.substring(0, requestPath.lastIndexOf('/'));
             else
                 return "/";
@@ -176,13 +176,11 @@ public class ThreadSafeCookieStore implements CookieStore {
 
         @Override
         public int compareTo(CookieKey o) {
+            Assertions.assertNotNull(o, "Parameter can't be null");
             int result;
-            if (o != null) {
-                if ((result = this.name.compareTo(o.name)) == 0)
-                    if ((result = this.domain.compareTo(o.domain)) == 0)
-                        result = this.path.compareTo(o.path);
-            } else
-                throw new NullPointerException();
+            if ((result = this.name.compareTo(o.name)) == 0)
+                if ((result = this.domain.compareTo(o.domain)) == 0)
+                    result = this.path.compareTo(o.path);
 
             return result;
         }
@@ -243,7 +241,7 @@ public class ThreadSafeCookieStore implements CookieStore {
 
         final boolean[] removeExpired = {false};
 
-        List<Cookie> result = cookieJar.entrySet().parallelStream().filter(pair -> {
+        List<Cookie> result = cookieJar.entrySet().stream().filter(pair -> {
             CookieKey key = pair.getKey();
             StoredCookie storedCookie = pair.getValue();
             boolean hasCookieExpired = hasCookieExpired(storedCookie.cookie, storedCookie.createdAt);
