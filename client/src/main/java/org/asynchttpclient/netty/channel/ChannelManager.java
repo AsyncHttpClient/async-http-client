AsyncHttpClient/async-http-client

commit 030e3dd4c7f3e49180724b72c59f0f660c1b4dcf

Drop PINNED_ENTRY (dead code)

client/src/main/java/org/asynchttpclient/netty/channel/ChannelManager.java
@@ -65,7 +65,6 @@ import java.util.stream.Collectors;
 
 public class ChannelManager {
 
-  public static final String PINNED_ENTRY = "entry";
   public static final String HTTP_CLIENT_CODEC = "http";
   public static final String SSL_HANDLER = "ssl";
   public static final String SOCKS_HANDLER = "socks";
@@ -208,22 +207,19 @@ public class ChannelManager {
     final AsyncHttpClientHandler httpHandler = new HttpHandler(config, this, requestSender);
     wsHandler = new WebSocketHandler(config, this, requestSender);
 
-    final NoopHandler pinnedEntry = new NoopHandler();
-
     final LoggingHandler loggingHandler = new LoggingHandler(LogLevel.TRACE);
 
     httpBootstrap.handler(new ChannelInitializer<Channel>() {
       @Override
       protected void initChannel(Channel ch) {
         ChannelPipeline pipeline = ch.pipeline()
-                .addLast(PINNED_ENTRY, pinnedEntry)
                 .addLast(HTTP_CLIENT_CODEC, newHttpClientCodec())
                 .addLast(INFLATER_HANDLER, newHttpContentDecompressor())
                 .addLast(CHUNKED_WRITER_HANDLER, new ChunkedWriteHandler())
                 .addLast(AHC_HTTP_HANDLER, httpHandler);
 
         if (LOGGER.isTraceEnabled()) {
-          pipeline.addAfter(PINNED_ENTRY, LOGGING_HANDLER, loggingHandler);
+          pipeline.addFirst(LOGGING_HANDLER, loggingHandler);
         }
 
         if (config.getHttpAdditionalChannelInitializer() != null)
@@ -235,7 +231,6 @@ public class ChannelManager {
       @Override
       protected void initChannel(Channel ch) {
         ChannelPipeline pipeline = ch.pipeline()
-                .addLast(PINNED_ENTRY, pinnedEntry)
                 .addLast(HTTP_CLIENT_CODEC, newHttpClientCodec())
                 .addLast(AHC_WS_HANDLER, wsHandler);
 
@@ -244,7 +239,7 @@ public class ChannelManager {
         }
 
         if (LOGGER.isDebugEnabled()) {
-          pipeline.addAfter(PINNED_ENTRY, LOGGING_HANDLER, loggingHandler);
+          pipeline.addFirst(LOGGING_HANDLER, loggingHandler);
         }
 
         if (config.getWsAdditionalChannelInitializer() != null)
@@ -347,12 +342,12 @@ public class ChannelManager {
       if (isSslHandlerConfigured(pipeline)) {
         pipeline.addAfter(SSL_HANDLER, HTTP_CLIENT_CODEC, newHttpClientCodec());
       } else {
-        pipeline.addAfter(PINNED_ENTRY, HTTP_CLIENT_CODEC, newHttpClientCodec());
-        pipeline.addAfter(PINNED_ENTRY, SSL_HANDLER, createSslHandler(requestUri.getHost(), requestUri.getExplicitPort()));
+        pipeline.addFirst(HTTP_CLIENT_CODEC, newHttpClientCodec());
+        pipeline.addFirst(SSL_HANDLER, createSslHandler(requestUri.getHost(), requestUri.getExplicitPort()));
       }
 
     else
-      pipeline.addAfter(PINNED_ENTRY, HTTP_CLIENT_CODEC, newHttpClientCodec());
+      pipeline.addFirst(HTTP_CLIENT_CODEC, newHttpClientCodec());
 
     if (requestUri.isWebSocket()) {
       pipeline.addAfter(AHC_HTTP_HANDLER, AHC_WS_HANDLER, wsHandler);
@@ -383,7 +378,7 @@ public class ChannelManager {
     if (hasSocksProxyHandler)
       pipeline.addAfter(SOCKS_HANDLER, SSL_HANDLER, sslHandler);
     else
-      pipeline.addAfter(PINNED_ENTRY, SSL_HANDLER, sslHandler);
+      pipeline.addFirst(SSL_HANDLER, sslHandler);
     return sslHandler;
   }
 
@@ -426,7 +421,7 @@ public class ChannelManager {
                 default:
                   throw new IllegalArgumentException("Only SOCKS4 and SOCKS5 supported at the moment.");
               }
-              channel.pipeline().addAfter(PINNED_ENTRY, SOCKS_HANDLER, socksProxyHandler);
+              channel.pipeline().addFirst(SOCKS_HANDLER, socksProxyHandler);
             }
           });
           promise.setSuccess(socksBootstrap);

client/src/main/java/org/asynchttpclient/netty/channel/NoopHandler.java
@@ -1,24 +1,0 @@
-/*
- * Copyright (c) 2015 AsyncHttpClient Project. All rights reserved.
- *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at
- *     http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
- */
-package org.asynchttpclient.netty.channel;
-
-import io.netty.channel.ChannelHandler.Sharable;
-import io.netty.channel.ChannelHandlerAdapter;
-
-/**
- * A noop handler that just serves as a pinned reference for adding and removing handlers in the pipeline
- */
-@Sharable
-public class NoopHandler extends ChannelHandlerAdapter {
-}

commit cce7a8ea6517ae9f6e60a870be0d181a2582f65b

ChannelManager#updatePipelineForHttpTunneling doesn't position handlers properly wrt additionalChannelInitializer, close #1529
Motivation:

030e3dd4c7f3e49180724b72c59f0f660c1b4dcf introduced a regression where
ChannelManager#updatePipelineForHttpTunneling uses `addFirst` to set
handlers in the pipeline. But additionalChannelInitializer might have
set handlers that require to be in front of the pipeline.

Modification:

Set position relative to AHC handler.

Result:

Proper handler positions

client/src/main/java/org/asynchttpclient/netty/channel/ChannelManager.java
@@ -338,16 +338,15 @@ public class ChannelManager {
     if (pipeline.get(HTTP_CLIENT_CODEC) != null)
       pipeline.remove(HTTP_CLIENT_CODEC);
 
-    if (requestUri.isSecured())
-      if (isSslHandlerConfigured(pipeline)) {
-        pipeline.addAfter(SSL_HANDLER, HTTP_CLIENT_CODEC, newHttpClientCodec());
-      } else {
-        pipeline.addFirst(HTTP_CLIENT_CODEC, newHttpClientCodec());
-        pipeline.addFirst(SSL_HANDLER, createSslHandler(requestUri.getHost(), requestUri.getExplicitPort()));
+    if (requestUri.isSecured()) {
+      if (!isSslHandlerConfigured(pipeline)) {
+        pipeline.addBefore(AHC_HTTP_HANDLER, SSL_HANDLER, createSslHandler(requestUri.getHost(), requestUri.getExplicitPort()));
       }
+      pipeline.addAfter(SSL_HANDLER, HTTP_CLIENT_CODEC, newHttpClientCodec());
 
-    else
-      pipeline.addFirst(HTTP_CLIENT_CODEC, newHttpClientCodec());
+    } else {
+      pipeline.addBefore(AHC_HTTP_HANDLER, HTTP_CLIENT_CODEC, newHttpClientCodec());
+    }
 
     if (requestUri.isWebSocket()) {
       pipeline.addAfter(AHC_HTTP_HANDLER, AHC_WS_HANDLER, wsHandler);