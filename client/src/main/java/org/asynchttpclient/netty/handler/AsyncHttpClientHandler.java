AsyncHttpClient/async-http-client

commit d5960563dcbd09854b8ae3e416108ce770b292c2

Remove dead code, there's no way we could receive something else

client/src/main/java/org/asynchttpclient/netty/handler/AsyncHttpClientHandler.java
@@ -67,15 +67,8 @@ public abstract class AsyncHttpClientHandler extends ChannelInboundHandlerAdapte
         Object attribute = Channels.getAttribute(channel);
 
         try {
-            if (attribute instanceof Callback) {
-                Callback ac = (Callback) attribute;
-                if (msg instanceof LastHttpContent) {
-                    ac.call();
-                } else if (!(msg instanceof HttpContent)) {
-                    logger.info("Received unexpected message while expecting a chunk: " + msg);
-                    ac.call();
-                    Channels.setDiscard(channel);
-                }
+            if (attribute instanceof Callback && msg instanceof LastHttpContent) {
+                ((Callback) attribute).call();
 
             } else if (attribute instanceof NettyResponseFuture) {
                 NettyResponseFuture<?> future = (NettyResponseFuture<?>) attribute;

commit c035ead9354f2957b180ea4f4c1fb2d479411f1b

Fix regression introduced in d5960563dcbd09854b8ae3e416108ce770b292c2, close #1274

client/src/main/java/org/asynchttpclient/netty/handler/AsyncHttpClientHandler.java
@@ -67,8 +67,10 @@ public abstract class AsyncHttpClientHandler extends ChannelInboundHandlerAdapte
     Object attribute = Channels.getAttribute(channel);
 
     try {
-      if (attribute instanceof OnLastHttpContentCallback && msg instanceof LastHttpContent) {
-        ((OnLastHttpContentCallback) attribute).call();
+      if (attribute instanceof OnLastHttpContentCallback) {
+        if (msg instanceof LastHttpContent) {
+          ((OnLastHttpContentCallback) attribute).call();
+        }
 
       } else if (attribute instanceof NettyResponseFuture) {
         NettyResponseFuture<?> future = (NettyResponseFuture<?>) attribute;