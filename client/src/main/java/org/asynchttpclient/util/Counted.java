1437c052388cd9a184dc0006320ab714060e6b08
sivaalli <siva_alli@hotmail.com>
2020-04-02 20:33:30 -0400

    added another test case and added docs on Counted

--- client/src/main/java/org/asynchttpclient/util/Counted.java
+++ client/src/main/java/org/asynchttpclient/util/Counted.java
@@ -1,13 +1,23 @@
 package org.asynchttpclient.util;
 
 /**
- * A contract that has methods to represent how many AHC instances
- * the implementation is shared with.
+ * An interface that defines useful methods to check how many {@linkplain org.asynchttpclient.AsyncHttpClient}
+ * instances this particular implementation is shared with.
  */
 public interface Counted {
+
+    /**
+     * Increment counter and return the incremented value
+     */
     int incrementAndGet();
 
+    /**
+     * Decrement counter and return the decremented value
+     */
     int decrementAndGet();
 
+    /**
+     * Return the current counter
+     */
     int count();
 }
