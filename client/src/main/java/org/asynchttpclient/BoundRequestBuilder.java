2ae98925571824adf8c3822b044d3da179a03105
Aayush Atharva <aayush@shieldblaze.com>
2024-07-30 02:18:33 +0530

    Prepare to release v3.0.0 (#1971)

--- client/src/main/java/org/asynchttpclient/BoundRequestBuilder.java
+++ client/src/main/java/org/asynchttpclient/BoundRequestBuilder.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2014-2023 AsyncHttpClient Project. All rights reserved.
+ *    Copyright (c) 2014-2024 AsyncHttpClient Project. All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
